31b0a1bbabc4a4e28cc5964b9b7cc024e71c71ac
Satyajeet-Das <dassatyajeet123@gmail.com>
2024-10-13 19:55:23 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Layout from "./layouts/Layout";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* If there is written anything inside the custom component it is passed as prop in children */}
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
